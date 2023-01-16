e01b183de80af13cfc1776ba8829ebcca363c2c3
Swittee Franca <190401022@live.unilag.edu.ng>
2023-01-16 01:41:43 +0100

    Pycodestyle corrections

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,31 +4,35 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """returns the string hello HBNB"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns the string HBNB"""
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """Returns the string c + whatever text is after"""
     return 'C {}'.format(text.replace("_", " "))
+
 
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     """Returns the string python + whatver text comes after it"""
     return "Python {}".format(text.replace("_", " "))
 
+
 @app.route('/python/', strict_slashes=False)
 def python():
     """Returns the string python is cool"""
     return "Python is cool"
-
 
 
 if __name__ == "__main__":
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -5,40 +5,48 @@ from flask import render_template
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """returns the string hello HBNB"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns the string HBNB"""
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """Returns the string c + whatever text is after"""
     return 'C {}'.format(text.replace("_", " "))
+
 
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     """Returns the string python + whatver text comes after it"""
     return "Python {}".format(text.replace("_", " "))
 
+
 @app.route('/python/', strict_slashes=False)
 def python():
     """Returns the string python is cool"""
     return "Python is cool"
 
+
 @app.route('/number/int:<n>', strict_slashes=False)
 def number(n):
-    """returns a statement that prints the value of n if int"""    
+    """returns a statement that prints the value of n if int"""
     return "{} is a number".format(n)
-    
+
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """renders a template that contains the value of n"""
     return render_template('5-number.html', n=n)
+
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
@@ -47,6 +55,7 @@ def odd_or_even(n):
         return render_template('6-number_odd_or_even.html', n=n, what="odd")
     else:
         return render_template('6-number_odd_or_even.html', n=n, what="even")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
